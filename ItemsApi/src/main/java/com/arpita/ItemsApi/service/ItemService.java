17ebb36a42a26518d411a7cb60a00714e42ca862
ArpitaSapkal <129717373+ArpitaSapkal@users.noreply.github.com>
2023-07-03 15:41:02 +0530

    Arpita Uploaded Final Project

---
+++ ItemsApi/src/main/java/com/arpita/ItemsApi/service/ItemService.java
@@ -0,0 +1,19 @@
+package com.arpita.ItemsApi.service;
+
+import com.arpita.ItemsApi.repository.entity.Item;
+
+import java.util.List;
+
+public interface ItemService
+{
+
+    Item save( Item item );
+
+    void delete( int itemId );
+
+    List<Item> all();
+
+    Item findById( int itemId );
+
+}
+
---
+++ ItemsApi/src/test/java/com/arpita/ItemsApi/ItemsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpita.ItemsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
